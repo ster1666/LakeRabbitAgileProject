999a02482072dcda30dec37412ed69c8c5fd73d9
ericljung <eric-ljung@telia.com>
2019-04-30 11:33:55 +0200

    UI Home page

--- app/src/main/java/com/example/lakebaikal/homeActivity.java
+++ app/src/main/java/com/example/lakebaikal/homeActivity.java
@@ -17,6 +17,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -24,11 +25,11 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
 import java.lang.reflect.Method;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
 
 import static com.example.lakebaikal.LoginActivity.bt_addr;
